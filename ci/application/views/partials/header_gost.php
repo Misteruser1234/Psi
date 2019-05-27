6d93481f9bc2cc01fa13d1007ffb2d657af67ec8
sm-io <48258579+sm-io@users.noreply.github.com>
2019-05-27 21:32:22 +0200

    Unutar foldera views dodat folder partials kao i footer.php i header_host.php partiali.

---
+++ ci/application/views/partials/header_gost.php
@@ -0,0 +1,59 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/nav.css">
+		<link rel="stylesheet" href="css/footer.css">
+		<link rel="stylesheet" href="css/login.css">
+		<title>Click and Chill</title>
+	</head>
+	<body>
+
+
+<!-- NAVIGACIJA -->
+
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
+	<a class="navbar-brand" href="#">Click and Chill</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<div class="mr-auto">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="#">Restorani</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Kafici</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Brza hrana</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Napredna pretraga</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">O nama</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Kontakt</a>
+			</li>
+		</ul>
+		</div>
+		<div class="login-btn">
+			<a class="btn btn-success my-2 my-sm-0 " href="#">Login</a>
+		</div>
+			
+	</div>
+</nav>
+
+<!-- END NAVIGACIJA -->
