47d6017ebd078ab64de34b0e998961bd2ea3361c
Misteruser1234 <48258470+Misteruser1234@users.noreply.github.com>
2019-05-29 22:52:33 +0200

    sad
    brt

--- ci/application/views/podesavanja-FormaPodaciUO.php
+++ ci/application/views/podesavanja-FormaPodaciUO.php
@@ -1,11 +1,11 @@
 <div class="my-5 mx-5">
 	<div class="h3 mt-5 mb-4">Podaci o ugostiteljskom objektu: </div>
-	<form>
+	<form metod="post" name="UOform" >
 		<!-- NAZIV -->
 		<div class="form-group row mb-1">
 		<label for="naziv" class="col-sm-4 col-form-label text-right">Naizv:</label>
 		<div class="col-sm-4">
-			<input type="text" class="form-control uo-polje" id="naziv" value="">
+			<input type="text" class="form-control uo-polje" id="naziv" name="naziv" value="" required>
 		</div>
 		</div>
 
@@ -13,7 +13,7 @@
 		<div class="form-group row mb-1">
 			<label for="adresa" class="col-sm-4 col-form-label text-right">Adresa:</label>
 			<div class="col-sm-4">
-				<input type="text" class="form-control uo-polje" id="adresa" value="">
+				<input type="text" class="form-control uo-polje" id="adresa" name="adresa" value="" required>
 			</div>
 		</div>
 
@@ -21,7 +21,7 @@
 		<div class="form-group row mb-1">
 			<label for="gmaps" class="col-sm-4 col-form-label text-right">Google maps:</label>
 			<div class="col-sm-4">
-				<input type="text" class="form-control uo-polje" id="gmaps" value="">
+				<input type="text" class="form-control uo-polje" id="gmaps"  name="mapa" value="" required>
 			</div>
 		</div>
 
@@ -30,15 +30,15 @@
 			<div class="h5 col-sm-4">Tip ugostiteljskog objekta: </div>
 			<div class="col-sm-8">
 				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="checkbox" id="restoran" value="1">
+					<input class="form-check-input" type="checkbox" id="restoran"  name="restoran" value="1">
 					<label class="form-check-label" for="restoran">Restoran</label>
 				</div>
 				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="checkbox" id="kafic" value="2">
+					<input class="form-check-input" type="checkbox" id="kafic" name="kafic" value="2">
 					<label class="form-check-label" for="kafic">Kafic</label>
 				</div>
 				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="checkbox" id="brza-hrana" value="3">
+					<input class="form-check-input" type="checkbox" id="brza-hrana"  name="brza" value="3">
 					<label class="form-check-label" for="brza-hrana">Brza hrana</label>
 				</div>
 			</div>
@@ -50,19 +50,19 @@
 			<div class="form-group row mb-1">
 				<label for="naziv" class="col-sm-4 col-form-label text-right">Pon - Pet:</label>
 				<div class="col-sm-4">
-					<input type="text" class="form-control uo-polje" id="pon-pet" value="">
+					<input type="text" class="form-control uo-polje" id="pon-pet" name="ponpet" value="">
 				</div>
 			</div>
 			<div class="form-group row mb-1">
 				<label for="adresa" class="col-sm-4 col-form-label text-right">Subota:</label>
 				<div class="col-sm-4">
-					<input type="text" class="form-control uo-polje" id="subota" value="">
+					<input type="text" class="form-control uo-polje" id="subota" name="subota" value="">
 				</div>
 			</div>
 			<div class="form-group row mb-1">
 				<label for="gmaps" class="col-sm-4 col-form-label text-right">Nedelja:</label>
 				<div class="col-sm-4">
-					<input type="text" class="form-control uo-polje" id="nedelja" value="">
+					<input type="text" class="form-control uo-polje" id="nedelja" name="nedelja" value="">
 				</div>
 			</div>
 		</div>
@@ -75,140 +75,140 @@
 					<div class="col-lg-3 col-md-6 my-3">
 						<h1 class="h3">Pice:</h1>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v1">
+							<input type="checkbox" class="form-check-input" id="s1v1" name="craft">
 							<label class="form-check-label" for="s1v1">Craft pivo</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v2">
+							<input type="checkbox" class="form-check-input" id="s1v2" name="kafaspec">
 							<label class="form-check-label" for="s1v2">Kafa special</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v3">
+							<input type="checkbox" class="form-check-input" id="s1v3" name="zestina">
 							<label class="form-check-label" for="s1v3">Zestina</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v4">
+							<input type="checkbox" class="form-check-input" id="s1v4" name="vina">
 							<label class="form-check-label" for="s1v4">Vina</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v5">
+							<input type="checkbox" class="form-check-input" id="s1v5" name="kokteli">
 							<label class="form-check-label" for="s1v5">Kokteli</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v6">
+							<input type="checkbox" class="form-check-input" id="s1v6" name="vitaminski">
 							<label class="form-check-label" for="s1v6">Vitaminski napici</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v7">
+							<input type="checkbox" class="form-check-input" id="s1v7" name="likeri">
 							<label class="form-check-label" for="s1v7">Likeri</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v8">
+							<input type="checkbox" class="form-check-input" id="s1v8" name="bezalkoholna">
 							<label class="form-check-label" for="s1v8">Bezalkoholna pica</label>
 						</div>
 					</div>
 					<div class="col-lg-3 col-md-6 my-3">
 						<h1 class="h3">Hrana:</h1>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v1">
+							<input type="checkbox" class="form-check-input" id="s2v1" name="kuvana">
 							<label class="form-check-label" for="s1v1">Kuvana jela</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v2">
+							<input type="checkbox" class="form-check-input" id="s2v2" name="dnevni">
 							<label class="form-check-label" for="s1v2">Dnevni meni</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v3">
+							<input type="checkbox" class="form-check-input" id="s2v3" name="rostilj">
 							<label class="form-check-label" for="s1v3">Rostilj</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v4">
+							<input type="checkbox" class="form-check-input" id="s2v4" name="pizza">
 							<label class="form-check-label" for="s1v4">Pizza</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v5">
+							<input type="checkbox" class="form-check-input" id="s2v5" name="salate">
 							<label class="form-check-label" for="s1v5">Obrok salate</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v6">
+							<input type="checkbox" class="form-check-input" id="s2v6" name="pecenje">
 							<label class="form-check-label" for="s1v6">Pecenje</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v7">
+							<input type="checkbox" class="form-check-input" id="s2v7" name="sushi">
 							<label class="form-check-label" for="s1v7">Sushi</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v8">
+							<input type="checkbox" class="form-check-input" id="s2v8" name="sendvici">
 							<label class="form-check-label" for="s1v8">Sendvici</label>
 						</div>
 					</div>
 					<div class="col-lg-3 col-md-6 my-3">
 						<h1 class="h3">Ambijent:</h1>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v1">
+							<input type="checkbox" class="form-check-input" id="s3v1" name="basta">
 							<label class="form-check-label" for="s1v1">Basta</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v2">
+							<input type="checkbox" class="form-check-input" id="s3v2" name="pogled">
 							<label class="form-check-label" for="s1v2">Pogled</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v3">
+							<input type="checkbox" class="form-check-input" id="s3v3" name="reka">
 							<label class="form-check-label" for="s1v3">Reka</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v4">
+							<input type="checkbox" class="form-check-input" id="s3v4" name="centar">
 							<label class="form-check-label" for="s1v4">Centar</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v5">
+							<input type="checkbox" class="form-check-input" id="s3v5" name="balkon">
 							<label class="form-check-label" for="s1v5">Balkon</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v6">
+							<input type="checkbox" class="form-check-input" id="s3v6" name="svirka">
 							<label class="form-check-label" for="s1v6">Ziva svirka</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v7">
+							<input type="checkbox" class="form-check-input" id="s3v7" name="chill">
 							<label class="form-check-label" for="s1v7">Chill</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v8">
+							<input type="checkbox" class="form-check-input" id="s3v8" name="splav">
 							<label class="form-check-label" for="s1v8">Splav</label>
 						</div>
 					</div>
 					<div class="col-lg-3 col-md-6 my-3">
 						<h1 class="h3">Extra:</h1>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v1">
+							<input type="checkbox" class="form-check-input" id="s4v1" name="wifi">
 							<label class="form-check-label" for="s1v1">WiFi</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v2">
+							<input type="checkbox" class="form-check-input" id="s4v2" name="pet">
 							<label class="form-check-label" for="s1v2">Pet Frendly</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v3">
+							<input type="checkbox" class="form-check-input" id="s4v3" name="parking">
 							<label class="form-check-label" for="s1v3">Parking</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v4">
+							<input type="checkbox" class="form-check-input" id="s4v4" name="baby">
 							<label class="form-check-label" for="s1v4">Baby oprema</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v5">
+							<input type="checkbox" class="form-check-input" id="s4v5" name="nosmoking">
 							<label class="form-check-label" for="s1v5">No Smoking zona</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v6">
+							<input type="checkbox" class="form-check-input" id="s4v6" name="dostava">
 							<label class="form-check-label" for="s1v6">Dostava</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v7">
+							<input type="checkbox" class="form-check-input" id="s4v7" name="happy">
 							<label class="form-check-label" for="s1v7">Happy hour</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v8">
+							<input type="checkbox" class="form-check-input" id="s4v8" name="tv">
 							<label class="form-check-label" for="s1v8">TV</label>
 						</div>
 					</div>
@@ -228,7 +228,7 @@
 							<img class= "img-fluid" src=" <?php echo base_url('img/restoran.jpg') ?> " alt="">
 						</div>
 						<div class="col-sm-12">
-								<input class="btn btn-primary py-2" type="file" style="width:inherit"></input>
+								<input class="btn btn-primary py-2" type="file" style="width:inherit" name="slika1"></input>
 						</div>
 					</div>
 				</div>
@@ -238,7 +238,7 @@
 							<img class= "img-fluid" src=" <?php echo base_url('img/restoran.jpg') ?> " alt="">
 						</div>
 						<div class="col-sm-12">
-								<input class="btn btn-primary py-2" type="file" style="width:inherit"></input>
+								<input class="btn btn-primary py-2" type="file" style="width:inherit"  name="slika2"></input>
 						</div>
 					</div>
 				</div>
@@ -248,7 +248,7 @@
 							<img class= "img-fluid" src=" <?php echo base_url('img/restoran.jpg') ?> " alt="">
 						</div>
 						<div class="col-sm-12">
-								<input class="btn btn-primary py-2" type="file" style="width:inherit"></input>
+								<input class="btn btn-primary py-2" type="file" style="width:inherit" name="slika3"></input>
 						</div>
 					</div>
 				</div>
@@ -262,7 +262,7 @@
 							<img class= "img-fluid" src=" <?php echo base_url('img/restoran.jpg') ?> " alt="">
 						</div>
 						<div class="col-sm-12">
-								<input class="btn btn-primary py-2" type="file" style="width:inherit"></input>
+								<input class="btn btn-primary py-2" type="file" style="width:inherit" name="slika4"></input>
 						</div>
 					</div>
 				</div>
@@ -272,7 +272,7 @@
 							<img class= "img-fluid" src=" <?php echo base_url('img/restoran.jpg') ?> " alt="">
 						</div>
 						<div class="col-sm-12">
-								<input class="btn btn-primary py-2" type="file" style="width:inherit"></input>
+								<input class="btn btn-primary py-2" type="file" style="width:inherit" name="slika5"></input>
 						</div>
 					</div>
 				</div>
@@ -282,7 +282,7 @@
 							<img class= "img-fluid" src=" <?php echo base_url('img/restoran.jpg') ?> " alt="">
 						</div>
 						<div class="col-sm-12">
-								<input class="btn btn-primary py-2" type="file" style="width:inherit"></input>
+								<input class="btn btn-primary py-2" type="file" style="width:inherit" name="slika6"></input>
 						</div>
 					</div>
 				</div>
@@ -296,7 +296,7 @@
 							<img class= "img-fluid" src=" <?php echo base_url('img/restoran.jpg') ?> " alt="">
 						</div>
 						<div class="col-sm-12">
-								<input class="btn btn-primary py-2" type="file" style="width:inherit"></input>
+								<input class="btn btn-primary py-2" type="file" style="width:inherit" name="slika7"></input>
 						</div>
 					</div>
 				</div>
@@ -306,7 +306,7 @@
 							<img class= "img-fluid" src=" <?php echo base_url('img/restoran.jpg') ?> " alt="">
 						</div>
 						<div class="col-sm-12">
-								<input class="btn btn-primary py-2" type="file" style="width:inherit"></input>
+								<input class="btn btn-primary py-2" type="file" style="width:inherit" name="slika8"></input>
 						</div>
 					</div>
 				</div>
@@ -316,7 +316,7 @@
 							<img class= "img-fluid" src=" <?php echo base_url('img/restoran.jpg') ?> " alt="">
 						</div>
 						<div class="col-sm-12">
-								<input class="btn btn-primary py-2" type="file" style="width:inherit"></input>
+								<input class="btn btn-primary py-2" type="file" style="width:inherit" name="slika9"></input>
 						</div>
 					</div>
 				</div>
@@ -326,30 +326,30 @@
 		<!-- OPIS -->
 		<div class="pt-5 pb-2">
 				<div class="h5 mb-3">Opis:</div>
-				<textarea class="w-100 ml-3" name="opis" id="opis" rows="10"></textarea>
+				<textarea class="w-100 ml-3" name="opis" id="opis" rows="10" name="opis"></textarea>
 		</div>
 
 		<!-- IZDVAJAMO SA MENIA -->
 		<div class="pt-5 pb-2">
 			<div class="h5 mb-3">Izdvajamo sa menia:</div>
-			<textarea class="w-100 ml-3" name="info1" id="info1" rows="10"></textarea>
+			<textarea class="w-100 ml-3" name="info1" id="info1" rows="10" name="samenija"></textarea>
 		</div>
 
 		<!-- PO CEMU SE RAZLIKUJEMO OD DRUGIH -->
 		<div class="pt-5 pb-2">
 				<div class="h5 mb-3">Po cemu se razlikujemo od drugih:</div>
-				<textarea class="w-100 ml-3" name="info2" id="info2" rows="10"></textarea>
+				<textarea class="w-100 ml-3" name="info2" id="info2" rows="10" name="razlike"></textarea>
 		</div>
 
 		<!-- ZASTO TREBA DA DODJETE KOD NAS -->
 		<div class="pt-5 pb-2">
 			<div class="h5 mb-3">Zasto treba da dodjete kod nas:</div>
-			<textarea class="w-100 ml-3" name="info3" id="info3" rows="10"></textarea>
+			<textarea class="w-100 ml-3" name="info3" id="info3" rows="10" name="zasto"></textarea>
 		</div>
 
 		<div class="row py-5">
 			<div class="col-md-6">
-					<button class="btn btn-primary w-100 my-3 py-3">
+					<button class="btn btn-primary w-100 my-3 py-3" href=" <?php echo site_url('Vlasnik/ubaciUO'); ?> ">
 						<i class="fas fa-save mx-2"></i>
 						Sacuvaj izmene
 					</button>
