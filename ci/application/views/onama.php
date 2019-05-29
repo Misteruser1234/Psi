ab437005fa1aec1d2a57a3974cc1ee0b7a06ac35
Viktoop <viktor.galindo95@gmail.com>
2019-05-29 12:54:46 +0200

    Kontakt ispravljen i o_nama picnut

--- ci/application/views/onama.php
+++ ci/application/views/onama.php
@@ -2,8 +2,8 @@
 	<div class="container">
 	<div class="naslov"> O Nama </div>
 		<div class="row mb-5">
-			<div class="col-md jumbotron jumbotron-fluid mx-3 px-3"> 
-				<div class= "container">
+			<div class="col-md onama-form jumbotron mx-3 px-3"> 
+				<div class= "container pb-0">
 					<h1 class="h3">
 						O Nama
 					</h1>
@@ -21,7 +21,7 @@
 					</ul>
 				</div>
 			</div>
-			<div class="col-md jumbotron jumbotron-fluid mx-3 px-3"> 
+			<div class="col-md jumbotron onama-form jumbotron-fluid mx-3 px-3"> 
 				<div class = "">
 					<div class= "container">
 						<h1 class="h3">
