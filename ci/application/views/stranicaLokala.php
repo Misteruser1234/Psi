bb6167067a6bac60d7032a29357fa6639682f5c9
sm-io <48258579+sm-io@users.noreply.github.com>
2019-05-31 00:51:19 +0200

    Stranica lokala fix I

--- ci/application/views/stranicaLokala.php
+++ ci/application/views/stranicaLokala.php
@@ -1,120 +1,204 @@
+<div class="container" style="border:1px solid black;">
+	<input type="hidden" id="uoid" name="uoid" value="0">
+	<!-- GALERIJA -->
+	<div class="row mt-5 mb-1 ml-2">
+		<div class="col-5 ">
+		</div>
+		<!-- INFO -->
+		<div class="col-7">
+			<div>
+				<div class="ml-5" >
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="h3" id="naziv-ocena-lokala">Naziv Lokala &#40 Ocena &#41</div>
+							<div class="row mt-4">
+								<div class="col-sm">
+									<div class="h4" id="ulica-lokala">Ulica i broj</div>
+								</div>
+							</div>
+							<div class="row mt-4 mb-1">
+								<div class="col-sm-4 ">
+									<button type="button" class="btn btn-success btn-block granica-dugme" id="restoran">Restoran</button>
+								</div>
+								<div class="col-sm-4">
+									<button type="button" class="btn btn-success btn-block granica-dugme" id="kafic">Kafic</button>
+								</div>
+								<div class="col-sm-4">
+									<button type="button" class="btn btn-success btn-block granica-dugme" id="brza">Brza Hrana</button>
+								</div>
+							</div>
+							<div class="row mt-4">
+								<div class="col-sm">
+									<div class="h4" id="radno">Radno vreme:</div>
+								</div>
+							</div>
+							<div class="row mt-4">
+								<div class="col-sm">
+									<div class="h5" id="radno-vreme-pon">pon-pet: od 09 do 23</div>
+								</div>
+							</div>
+							<div class="row mt-4">
+								<div class="col-sm">
+									<div class="h5" id="radno-vreme-pon">sub: od 09 do 23</div>
+								</div>
+							</div>
+							<div class="row mt-4">
+								<div class="col-sm">
+									<div class="h5" id="radno-vreme-pon">ned: od 09 do 23</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6"><img class="img-fluid padding-right-5" src="<?php echo base_url('img/restoran.jpg');?>"  alt="First slide"></div>
+					</div>
+					
+					<!-- TAGOVI -->
+					<div class="row mt-2">
+						<div class="col-sm-1 h3 mt-2">Tagovi:</div>
+						<div class="col-sm-10 mt-2 ml-5">
+							<div class="row">
+								<div class="col-sm-3 "><button type="button" class="btn btn-light btn-block granica-dugme-2" id="tag1">Tag1</button></div>
+								<div class="col-sm-3"><button type="button" class="btn btn-light btn-block granica-dugme-2" id="tag2">Tag2</button></div>
+								<div class="col-sm-3"><button type="button" class="btn btn-light btn-block granica-dugme-2" id="tag3">Tag3</button></div>
+								<div class="col-sm-3"><button type="button" class="btn btn-light btn-block granica-dugme-2" id="tag4">Tag4</button></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- HEADER STRANICE LOKALA -->
 <div class="container">
-<input type="hidden" id="uoid" name="uoid" value="0">
-		<!-- GALERIJA -->
-		<div class="row mt-5 mb-1 ml-2">
-			 <div class="col-5 " >
+	<div class="row rez-uo rez-form jumbotron py-3" >
+		<div class="row">
+
+			<!-- LEVI DEO ZAGLAVLJA -->
+			<div class="col-md-5 pr-5 pl-0">
+				<!-- CAROUSEL -->
 				<div id="carouselExampleControls" class="carousel slide  granica" data-ride="carousel" data-interval="false">
 					<div class="carousel-inner">
-				<div class="carousel-item active">
-					<img class="d-block w-100" src="<?php echo base_url('img/restoran.jpg');?>" alt="First slide">
-				</div>
-				<div class="carousel-item">
-				<img class="d-block w-100" src="<?php echo base_url('img/restoran.jpg');?>" alt="Second slide">
-				</div>
-					<div class="carousel-item">
-				<img class="d-block w-100" src="<?php echo base_url('img/restoran.jpg');?>" alt="Third slide">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="<?php echo base_url('img/restoran.jpg');?>" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="<?php echo base_url('img/restoran.jpg');?>" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="<?php echo base_url('img/restoran.jpg');?>" alt="Third slide">
+						</div>
 					</div>
-				</div>
-				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 						<span class="sr-only">Previous</span>
-							</a>
-						<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-							<span class="carousel-control-next-icon" aria-hidden="true"></span>
-								<span class="sr-only">Next</span>
-				</a>
-					</div>
-
-			 
-			 </span>
-			 </div>
-			<!-- INFO -->
-			<div class="col-7">
-				<div>
-					<div class="ml-5" >
-						<div class="row">
-							<div class="col-sm-6">
-								<div class="h3" id="naziv-ocena-lokala">Naziv Lokala &#40 Ocena &#41</div>
-								<div class="row mt-4">
-							<div class="col-sm">
-								<div class="h4" id="ulica-lokala">Ulica i broj</div>
-							</div>
-							
-							
-						</div>
-						<div class="row mt-4 mb-1">
-							<div class="col-sm-4 ">
-								<button type="button" class="btn btn-success btn-block granica-dugme" id="restoran">Restoran</button>
-							</div>
-							<div class="col-sm-4">
-									<button type="button" class="btn btn-success btn-block granica-dugme" id="kafic">Kafic</button>
-							</div>
-							<div class="col-sm-4">
-									<button type="button" class="btn btn-success btn-block granica-dugme" id="brza">Brza Hrana</button>
-							</div>
-							
-							
-						</div>
-						
-						<div class="row mt-4">
-							<div class="col-sm">
-								<div class="h4" id="radno">Radno vreme:</div>
-							</div>
-							
-							
-						</div>
-						<div class="row mt-4">
-							<div class="col-sm">
-								<div class="h5" id="radno-vreme-pon">pon-pet: od 09 do 23</div>
-							</div>
-							
-							
-						</div>
-						<div class="row mt-4">
-							<div class="col-sm">
-								<div class="h5" id="radno-vreme-pon">sub: od 09 do 23</div>
-							</div>
-							
-							
-						</div>
-						<div class="row mt-4">
-							<div class="col-sm">
-								<div class="h5" id="radno-vreme-pon">ned: od 09 do 23</div>
-							</div>
-							
-							
-						</div>
-							</div>
-							<div class="col-sm-6"><img class="img-fluid padding-right-5" src="<?php echo base_url('img/restoran.jpg');?>"  alt="First slide"></div>
-							
-							
-							
-						</div>
-						
-						<!-- TAGOVI -->
-						<div class="row mt-2">
-							<div class="col-sm-1 h3 mt-2">Tagovi:</div>
-							<div class="col-sm-10 mt-2 ml-5">
-								<div class="row">
-									<div class="col-sm-3 "><button type="button" class="btn btn-light btn-block granica-dugme-2" id="tag1">Tag1</button></div>
-									<div class="col-sm-3"><button type="button" class="btn btn-light btn-block granica-dugme-2" id="tag2">Tag2</button></div>
-									<div class="col-sm-3"><button type="button" class="btn btn-light btn-block granica-dugme-2" id="tag3">Tag3</button></div>
-									<div class="col-sm-3"><button type="button" class="btn btn-light btn-block granica-dugme-2" id="tag4">Tag4</button></div>
-								</div>
-						
-							</div>
-						
-						</div>
-						
-						
-					</div>
-				
+					</a>
+					<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
 				</div>
-			
+				<!-- END CAROUSEL -->
+			</div>
+			<!-- END LEVI DEO ZAGLAVLJA -->
+
+
+			<!-- DESNI DEO ZAGLAVLJA -->
+			<div class="col-md-7 mb-8 px">
+				
+				<!-- NAZIV LOKALA I OCENA -->
+				<div class="row">
+					<div class="h4">
+						Naziv lokala
+						<span class="tag h6 py-1 text-white ml-2 px-3 bg-success">XX.X</span>	
+					</div>
+				</div>
+				<!-- END NAZIV LOKALA I OCENA -->
+				
+				<div class="row">
+					<div class="col-sm-7">
+						<!-- ADRESA -->
+							<div class="row mb-2">
+								<div class="h6">Adresa XX</div>
+							</div>	
+						<!-- END ADRESA -->
+
+						<!-- TAG VRSTA UO -->
+						<div class="row mb-2">
+							<span class="tag mr-2 px-3 bg-primary text-white">Kafić</span>
+							<span class="tag mr-2 px-3 bg-light">Restoran</span>
+							<span class="tag mr-2 px-3 bg-light">Brza hrana</span>
+						</div>
+						<!-- END TAG VRSTA UO -->
+
+						<!-- RADNO VREME BLOCK -->
+						<div class="row">
+							<div class="h5">Radno vreme:</div>
+						</div>
+						<div class="row mb-2">
+							<div class="h6 col-sm-6">pon-pet:</div>
+							<div class="h6 col-sm-6">9:00 - 23:00</div>
+							<div class="h6 col-sm-6">subota:</div>
+							<div class="h6 col-sm-6">9:00 - 23:00</div>
+							<div class="h6 col-sm-6">nedelja:</div>
+							<div class="h6 col-sm-6">9:00 - 23:00</div>
+						</div>
+						<!-- END RADNO VREME BLOCK -->
+					</div>
+					<div class="col-sm-5">
+						<div class="">
+							<img class="img-fluid img-thumbnail w-100"  src="<?php echo base_url('img/restoran.jpg');?>" alt>
+						</div>
+					</div>
+				</div>
+				<!-- TAGOVI -->
+				<div class="row mt-2">
+					<span class="h5 mr-2">Tagovi:</span>						
+					<span class="tag text-white bg-info h6 mr-1 px-3">Bezalkoholna pica</span>
+					<span class="tag text-white bg-info h6 mr-1 px-3">Likeri</span>
+					<span class="tag text-white bg-info h6 mr-1 px-3">Vitaminski napici</span>
+					<span class="tag text-white bg-info h6 mr-1 px-3">Kokteli</span>
+					<span class="tag text-white bg-info h6 mr-1 px-3">Vina</span>
+					<span class="tag text-white bg-info h6 mr-1 px-3">Zestina</span>
+					<span class="tag text-white bg-info h6 mr-1 px-3">Kafa special</span>
+					<span class="tag text-white bg-info h6 mr-1 px-3">Craft pivo</span>
+				</div>
+				<!-- END TAGOVI -->
+
+				<div class="col-sm-12">
+					<div class="col-sm">
+						<div class="row">
+							<div class=" col-sm-5">
+								
+								
+							</div>	
+							
+							<div class="col-sm-7">
+								
+								
+							</div>		
+						</div>
+						<div class="row">
+							<div class="col-sm-5">
+
+							
+
+							</div>
+							
+							<div class="col-sm-7">
+								
+								
+							</div>
+						</div>
+					</div>	
+				</div>
 			</div>
 		</div>
 		
-		
+	</div>
+</div>
+
+<div class="container" style="border:1px solid black;">
 		<!--THUMBNAILOVI-->
 		
 		<div class="row ml-5 mr-5 mt-5 mb-5 justify-content-center">
