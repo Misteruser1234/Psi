105fe853c2ed9da14cf2ba994becc485de42f1f1
Viktoop <viktor.galindo95@gmail.com>
2019-06-01 17:59:27 +0200

    Fix / u .

--- ci/application/views/partials/podesavanja-spisakUO-single-uo.php
+++ ci/application/views/partials/podesavanja-spisakUO-single-uo.php
@@ -2,5 +2,5 @@
 				<div class="col-9"><?php echo $data->Naziv?></div>
 				<div class="col-1"> <a href="<?php echo site_url('Gost/stranica_lokala/'.$data->IDUO);?>"><i class="fas fa-eye"></i> </a> </div>
 				<div class="col-1"> <a href="#"><i class="fas fa-edit"></i> </a> </div>
-				<div class="col-1"> <a href="<?php echo site_url('Vlasnik/brisi_UO'.$data->IDUO); ?>"><i class="far fa-trash-alt"></i></a> </div>
+				<div class="col-1"> <a href="<?php echo site_url('Vlasnik/brisi_UO/'.$data->IDUO); ?>"><i class="far fa-trash-alt"></i></a> </div>
 			</div>
