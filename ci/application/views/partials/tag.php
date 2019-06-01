86bd5acac68a62dcfc0b7f813479af8e7aba265d
sm-io <48258579+sm-io@users.noreply.github.com>
2019-06-01 04:49:14 +0200

    partials/tag.php fix za error ako nema tagova za prikaz

--- ci/application/views/partials/tag.php
+++ ci/application/views/partials/tag.php
@@ -1,4 +1,7 @@
-<?php foreach ( $tagovi as $tag){
- echo('<span class="tag text-white bg-info h6 mr-1 px-3">'); echo $tag; echo '</span>';
-}
+<?php 
+    if ($tagovi){
+        foreach ( $tagovi as $tag) {
+            echo('<span class="tag text-white bg-info h6 mr-1 px-3">'); echo $tag; echo '</span>';
+        }
+    }
 ?>
