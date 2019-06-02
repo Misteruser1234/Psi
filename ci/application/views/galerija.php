497ca9725633f512a0f71ea1041a1b346deead87
sm-io <48258579+sm-io@users.noreply.github.com>
2019-06-02 05:40:09 +0200

    Galerija gotova

--- ci/application/views/galerija.php
+++ ci/application/views/galerija.php
@@ -2,20 +2,20 @@
     <div class="glavna-slika-frame">
         <i id="strelica-desno" class="fas fa-chevron-right"></i>
         <i id="strelica-levo" class="fas fa-chevron-left"></i>
-        <img src="<?php echo base_url('/img/restoran.jpg')?>" alt="" class="img-fluid glavna-slika" >
+        <img src="" alt="" pos=0 class="img-fluid glavna-slika" >
     </div>
     <div class="slike-dole">
         <div class="container mt-3" style="border-top: 1px solid #aaa;">
             <div class="row my-3">
-                <div class="col"><img src="<?php echo base_url('img/restoran.jpg');?>" class="img-fluid slicica" alt="" id="thumb-1"></div>
-                <div class="col"><img src="<?php echo base_url('img/restoran.jpg');?>" class="img-fluid slicica" alt="" id="thumb-2"></div>
-                <div class="col"><img src="<?php echo base_url('img/restoran.jpg');?>" class="img-fluid slicica" alt="" id="thumb-3"></div>
-                <div class="col"><img src="<?php echo base_url('img/restoran.jpg');?>" class="img-fluid slicica" alt="" id="thumb-4"></div>
-                <div class="col"><img src="<?php echo base_url('img/restoran.jpg');?>" class="img-fluid slicica" alt="" id="thumb-5"></div>
-                <div class="col"><img src="<?php echo base_url('img/restoran.jpg');?>" class="img-fluid slicica" alt="" id="thumb-6"></div>
-                <div class="col"><img src="<?php echo base_url('img/restoran.jpg');?>" class="img-fluid slicica" alt="" id="thumb-7"></div>
-                <div class="col"><img src="<?php echo base_url('img/restoran.jpg');?>" class="img-fluid slicica" alt="" id="thumb-8"></div>
-                <div class="col"><img src="<?php echo base_url('img/restoran.jpg');?>" class="img-fluid slicica" alt="" id="thumb-9"></div>
+                <div class="col"><img src="" pos="0" class="img-fluid g-slika" alt=""></div>
+                <div class="col"><img src="" pos="1" class="img-fluid g-slika" alt=""></div>
+                <div class="col"><img src="" pos="2" class="img-fluid g-slika" alt=""></div>
+                <div class="col"><img src="" pos="3" class="img-fluid g-slika" alt=""></div>
+                <div class="col"><img src="" pos="4" class="img-fluid g-slika" alt=""></div>
+                <div class="col"><img src="" pos="5" class="img-fluid g-slika" alt=""></div>
+                <div class="col"><img src="" pos="6" class="img-fluid g-slika" alt=""></div>
+                <div class="col"><img src="" pos="7" class="img-fluid g-slika" alt=""></div>
+                <div class="col"><img src="" pos="8" class="img-fluid g-slika" alt=""></div>
             </div>
             <!--END THUMBNAILOVI-->
         </div>
--- ci/js/galerija.js
+++ ci/js/galerija.js
@@ -1,5 +1,6 @@
 $("img.d-block.w-100").click(function(){
-//$(document).ready(function(){    
+    $("body").css({"overflow":"hidden", "margin-right":"15px"});
+    $(".maska").show();   
     galerijaInit();
 });
 
@@ -21,8 +22,15 @@ $(".maska i").click(function(e) {
 });
 
 function galerijaInit(){
-    $("body").css({"overflow":"hidden", "margin-right":"15px"});
-    $(".maska").show();
+
+    //Dinamicki ucitava link prve slike sa stranice UO u glavnu sliku
+    $(".glavna-slika").attr('src', $( $(".uo-slika")[0] ).attr('src') );
+
+    // Dinamicki ucitava linkove slika na dnu galirije od slika sa stranice UO
+    for (i=0; i<9; i++) $( $(".g-slika")[i] ).attr('src', $( $(".uo-slika")[i] ).attr('src') );
+
+
+    // Postavlja width glavne slike u odnosu na dimenzije slike i dimenzije ekrana
     slikeDole = $(".slike-dole");
     slika = $(".glavna-slika"); //.width('30vw');
 
@@ -37,10 +45,12 @@ function galerijaInit(){
     if (slikaRatio >= ekranRatio) slika.outerWidth( wSpace * faktor );
     else slika.outerWidth( hSpace * faktor * slikaRatio);
 
+    // Centrira glavnu sliku na ekranu
     slikaTop  = (hSpace - slika.outerHeight()) / 2;
     slikaLeft = (wSpace - slika.outerWidth() ) / 2;
     slika.css({"top": slikaTop + "px", "left": slikaLeft + "px"});
 
+    // Pozicionira strelice i odredjuje im velicinu
     levo  = $("#strelica-levo");
     desno = $("#strelica-desno");
 
@@ -51,3 +61,26 @@ function galerijaInit(){
     desno.css({"top": slikaTop + (slika.outerHeight() - desno.outerHeight()) / 2 + "px", "left": slikaLeft + slika.outerWidth() + 2*desno.outerWidth() + "px"});
 
 }
+
+// Postavlja src glavne slike da budi isti kao i src slike na dnu na koju je kliknuto
+$(".g-slika").click(function(){
+    $(".glavna-slika").attr('src', $(this).attr('src') );
+    $(".glavna-slika").attr('pos', $(this).attr('pos') );
+});
+
+// Menja glavnu sliku u levo u odnosu na slilke na dnu
+$("#strelica-desno").click(function(){
+    pos = parseInt( $(".glavna-slika").attr('pos'), 10);
+    pos = (pos + 1) % 9;
+    $(".glavna-slika").attr('src', $( $(".uo-slika")[pos] ).attr('src') );
+    $(".glavna-slika").attr('pos', pos);
+});
+
+// Menja glavnu sliku u desno u odnosu na slilke na dnu
+$("#strelica-levo").click(function(){
+    pos = parseInt( $(".glavna-slika").attr('pos'), 10);
+    pos = pos - 1;
+    if (pos == -1) pos = 8;
+    $(".glavna-slika").attr('src', $( $(".uo-slika")[pos] ).attr('src') );
+    $(".glavna-slika").attr('pos', pos);
+});
