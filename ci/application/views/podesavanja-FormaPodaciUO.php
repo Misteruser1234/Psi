cfe613bf16635801cf0648669eb3667d861b4b19
micisogi <49591307+micisogi@users.noreply.github.com>
2019-06-04 21:19:55 +0200

    fix tagova i napredne pretrage slika

--- ci/application/views/podesavanja-FormaPodaciUO.php
+++ ci/application/views/podesavanja-FormaPodaciUO.php
@@ -259,140 +259,140 @@ function previewImage(idslike,imefajla) {
 					<div class="col-lg-3 col-md-6 my-3">
 						<h1 class="h3">Pice:</h1>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v1" name="craft"<?php if($data['id'] != NULL) echo $tag['pice'][0]==1?'checked':'';?>  value="1">
+							<input type="checkbox" class="form-check-input" id="s1v1" name="craft"<?php if($data['id'] != NULL) echo $tag['pice'][7]==1?'checked':'';?>  value="1">
 							<label class="form-check-label" for="s1v1">Craft pivo</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v2" name="kafaspec"<?php if($data['id'] != NULL) echo $tag['pice'][1]==1?'checked':'';?>  value="1">
+							<input type="checkbox" class="form-check-input" id="s1v2" name="kafaspec"<?php if($data['id'] != NULL) echo $tag['pice'][6]==1?'checked':'';?>  value="1">
 							<label class="form-check-label" for="s1v2">Kafa special</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v3" name="zestina"<?php if($data['id'] != NULL) echo $tag['pice'][2]==1?'checked':'';?>  value="1">
+							<input type="checkbox" class="form-check-input" id="s1v3" name="zestina"<?php if($data['id'] != NULL) echo $tag['pice'][5]==1?'checked':'';?>  value="1">
 							<label class="form-check-label" for="s1v3">Zestina</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v4" name="vina"<?php if($data['id'] != NULL) echo $tag['pice'][3]==1?'checked':'';?>  value="1">
+							<input type="checkbox" class="form-check-input" id="s1v4" name="vina"<?php if($data['id'] != NULL) echo $tag['pice'][4]==1?'checked':'';?>  value="1">
 							<label class="form-check-label" for="s1v4">Vina</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v5" name="kokteli"<?php if($data['id'] != NULL) echo $tag['pice'][4]==1?'checked':'';?>  value="1">
+							<input type="checkbox" class="form-check-input" id="s1v5" name="kokteli"<?php if($data['id'] != NULL) echo $tag['pice'][3]==1?'checked':'';?>  value="1">
 							<label class="form-check-label" for="s1v5">Kokteli</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v6" name="vitaminski"<?php if($data['id'] != NULL) echo $tag['pice'][5]==1?'checked':'';?>  value="1">
+							<input type="checkbox" class="form-check-input" id="s1v6" name="vitaminski"<?php if($data['id'] != NULL) echo $tag['pice'][2]==1?'checked':'';?>  value="1">
 							<label class="form-check-label" for="s1v6">Vitaminski napici</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v7" name="likeri"<?php if($data['id'] != NULL) echo $tag['pice'][6]==1?'checked':'';?>  value="1">
+							<input type="checkbox" class="form-check-input" id="s1v7" name="likeri"<?php if($data['id'] != NULL) echo $tag['pice'][1]==1?'checked':'';?>  value="1">
 							<label class="form-check-label" for="s1v7">Likeri</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s1v8" name="bezalkoholna"<?php if($data['id'] != NULL) echo $tag['pice'][7]==1?'checked':'';?>  value="1">
+							<input type="checkbox" class="form-check-input" id="s1v8" name="bezalkoholna"<?php if($data['id'] != NULL) echo $tag['pice'][0]==1?'checked':'';?>  value="1">
 							<label class="form-check-label" for="s1v8">Bezalkoholna pica</label>
 						</div>
 					</div>
 					<div class="col-lg-3 col-md-6 my-3">
 						<h1 class="h3">Hrana:</h1>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v1" name="kuvana"<?php if($data['id'] != NULL) echo $tag['hrana'][0]==1?'checked':'';?>  value="1">
+							<input type="checkbox" class="form-check-input" id="s2v1" name="kuvana"<?php if($data['id'] != NULL) echo $tag['hrana'][7]==1?'checked':'';?>  value="1">
 							<label class="form-check-label" for="s2v1">Kuvana jela</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v2" name="dnevni"<?php if($data['id'] != NULL) echo $tag['hrana'][1]==1?'checked':'';?>  value="1">
+							<input type="checkbox" class="form-check-input" id="s2v2" name="dnevni"<?php if($data['id'] != NULL) echo $tag['hrana'][6]==1?'checked':'';?>  value="1">
 							<label class="form-check-label" for="s2v2">Dnevni meni</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v3" name="rostilj"<?php if($data['id'] != NULL) echo $tag['hrana'][2]==1?'checked':'';?>  value="1">
+							<input type="checkbox" class="form-check-input" id="s2v3" name="rostilj"<?php if($data['id'] != NULL) echo $tag['hrana'][5]==1?'checked':'';?>  value="1">
 							<label class="form-check-label" for="s2v3">Rostilj</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v4" name="pizza"<?php if($data['id'] != NULL) echo $tag['hrana'][3]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s2v4" name="pizza"<?php if($data['id'] != NULL) echo $tag['hrana'][4]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s2v4">Pizza</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v5" name="salate"<?php if($data['id'] != NULL) echo $tag['hrana'][4]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s2v5" name="salate"<?php if($data['id'] != NULL) echo $tag['hrana'][3]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s2v5">Obrok salate</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v6" name="pecenje"<?php if($data['id'] != NULL) echo $tag['hrana'][5]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s2v6" name="pecenje"<?php if($data['id'] != NULL) echo $tag['hrana'][2]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s2v6">Pecenje</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v7" name="sushi"<?php if($data['id'] != NULL) echo $tag['hrana'][6]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s2v7" name="sushi"<?php if($data['id'] != NULL) echo $tag['hrana'][1]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s2v7">Sushi</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s2v8" name="sendvici"<?php if($data['id'] != NULL) echo $tag['hrana'][7]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s2v8" name="sendvici"<?php if($data['id'] != NULL) echo $tag['hrana'][0]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s2v8">Sendvici</label>
 						</div>
 					</div>
 					<div class="col-lg-3 col-md-6 my-3">
 						<h1 class="h3">Ambijent:</h1>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v1" name="basta"<?php if($data['id'] != NULL) echo $tag['ambijent'][0]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s3v1" name="basta"<?php if($data['id'] != NULL) echo $tag['ambijent'][7]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s3v1">Basta</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v2" name="pogled"<?php if($data['id'] != NULL) echo $tag['ambijent'][1]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s3v2" name="pogled"<?php if($data['id'] != NULL) echo $tag['ambijent'][6]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s3v2">Pogled</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v3" name="reka"<?php if($data['id'] != NULL) echo $tag['ambijent'][2]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s3v3" name="reka"<?php if($data['id'] != NULL) echo $tag['ambijent'][5]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s3v3">Reka</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v4" name="centar"<?php if($data['id'] != NULL) echo $tag['ambijent'][3]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s3v4" name="centar"<?php if($data['id'] != NULL) echo $tag['ambijent'][4]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s3v4">Centar</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v5" name="balkon"<?php if($data['id'] != NULL) echo $tag['ambijent'][4]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s3v5" name="balkon"<?php if($data['id'] != NULL) echo $tag['ambijent'][3]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s3v5">Balkon</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v6" name="svirka"<?php if($data['id'] != NULL) echo $tag['ambijent'][5]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s3v6" name="svirka"<?php if($data['id'] != NULL) echo $tag['ambijent'][2]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s3v6">Ziva svirka</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v7" name="chill"<?php if($data['id'] != NULL) echo $tag['ambijent'][6]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s3v7" name="chill"<?php if($data['id'] != NULL) echo $tag['ambijent'][1]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s3v7">Chill</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s3v8" name="splav"<?php if($data['id'] != NULL) echo $tag['ambijent'][7]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s3v8" name="splav"<?php if($data['id'] != NULL) echo $tag['ambijent'][0]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s3v8">Splav</label>
 						</div>
 					</div>
 					<div class="col-lg-3 col-md-6 my-3">
 						<h1 class="h3">Extra:</h1>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v1" name="wifi"<?php if($data['id'] != NULL) echo $tag['ekstra'][0]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s4v1" name="wifi"<?php if($data['id'] != NULL) echo $tag['ekstra'][7]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s4v1">WiFi</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v2" name="pet"<?php if($data['id'] != NULL) echo $tag['ekstra'][1]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s4v2" name="pet"<?php if($data['id'] != NULL) echo $tag['ekstra'][6]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s4v2">Pet Frendly</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v3" name="parking" <?php if($data['id'] != NULL) echo $tag['ekstra'][2]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s4v3" name="parking" <?php if($data['id'] != NULL) echo $tag['ekstra'][5]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s4v3">Parking</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v4" name="baby" <?php if($data['id'] != NULL) echo $tag['ekstra'][3]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s4v4" name="baby" <?php if($data['id'] != NULL) echo $tag['ekstra'][4]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s4v4">Baby oprema</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v5" name="nosmoking"<?php if($data['id'] != NULL) echo $tag['ekstra'][4]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s4v5" name="nosmoking"<?php if($data['id'] != NULL) echo $tag['ekstra'][3]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s4v5">No Smoking zona</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v6" name="dostava"<?php if($data['id'] != NULL) echo $tag['ekstra'][5]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s4v6" name="dostava"<?php if($data['id'] != NULL) echo $tag['ekstra'][2]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s4v6">Dostava</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v7" name="happy"<?php if($data['id'] != NULL) echo $tag['ekstra'][6]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s4v7" name="happy"<?php if($data['id'] != NULL) echo $tag['ekstra'][1]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s4v7">Happy hour</label>
 						</div>
 						<div class="form-check np-polje">
-							<input type="checkbox" class="form-check-input" id="s4v8" name="tv"<?php if($data['id'] != NULL) echo $tag['ekstra'][7]==1?'checked':'';?> value="1">
+							<input type="checkbox" class="form-check-input" id="s4v8" name="tv"<?php if($data['id'] != NULL) echo $tag['ekstra'][0]==1?'checked':'';?> value="1">
 							<label class="form-check-label" for="s4v8">TV</label>
 						</div>
 					</div>
